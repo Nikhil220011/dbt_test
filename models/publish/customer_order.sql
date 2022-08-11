bd974e97bc21d6830e5e390be4e297707fde0b9f
nikhil.barya@hashmapinc.com <nikhil.barya@hashmapinc.com>
2022-08-11 15:31:11 +0000

    commit1

---
+++ models/publish/customer_order.sql
@@ -0,0 +1,9 @@
+{{ config( alias= 'customer_orders',
+    schema= 'PUBLISH',
+    database= 'RAW' 
+)}}
+
+SELECT c.*,o.*
+FROM {{ source('STAGE', 'CUSTOMER') }} as c
+    left join {{ source('STAGE', 'ORDERS') }} as o
+        on c.C_CUSTKEY=o.O_CUSTKEY
